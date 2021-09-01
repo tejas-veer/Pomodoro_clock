bb04167ae879a7ac8adeedbca6dfaa7ba087ca34
tejas-veer <tejasveer41@gmail.com>
2021-09-01 14:21:45 +0530

    done

--- src/index.js
+++ src/index.js
@@ -213,7 +213,7 @@ class App1 extends React.Component {
     return (
       <div className="pomodoro-container">
         {/* <nav id="navbar"><span><img src="https://cdn.pixabay.com/photo/2017/11/10/13/32/clock-2936333_1280.png"></img></span>Focus Clock</nav> */}
-        <div className="clock-container" style={{ boxShadow: this.state.isPlaying ? '0px 0px 50px gray' : '0px 0px 0px gray' }}>
+        <div className="clock-container" >
           <h1>{currentState}</h1>
           <span>{this.convertToTime(currentTime)}</span>
         </div>
